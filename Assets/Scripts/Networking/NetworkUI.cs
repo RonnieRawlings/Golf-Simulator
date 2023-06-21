0b8860907f96732967242e35d8225b00ab7e83e9
Ronnie Rawlings <rr258736@falmouth.ac.uk>
2023-06-21 21:40:48 +0100

    Added/Implemented - TMP Packages + Client/Host/Server Buttons
    This commit adds the TMP packages so that I can use the TextMeshProUGUI buttons. The buttons to start a client/host or server has also been implemented.
    
    Next, continue to work on the multiplayer functionality.

---
+++ Assets/Scripts/Networking/NetworkUI.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NetworkUI : MonoBehaviour
+{
+    [SerializeField] private Button serverBtn, hostBtn, clientBtn;
+
+    private void Awake()
+    {
+        serverBtn.onClick.AddListener(() =>
+        {
+            NetworkManager.Singleton.StartServer();
+        });
+
+        hostBtn.onClick.AddListener(() =>
+        {
+            NetworkManager.Singleton.StartHost();
+        });
+
+        clientBtn.onClick.AddListener(() =>
+        {
+            NetworkManager.Singleton.StartClient();
+        });
+    }
+}
